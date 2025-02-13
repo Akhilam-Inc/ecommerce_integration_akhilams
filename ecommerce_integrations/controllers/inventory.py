d7f80c19c1e88ed2cc895758871bd9eea17dfd5c
Navin-S-R <navin@aerele.in>
2025-02-13 12:05:03 +0530

    fix: Order the inventory sync to sync the oldest synced items first

--- ecommerce_integrations/controllers/inventory.py
+++ ecommerce_integrations/controllers/inventory.py
@@ -56,10 +56,9 @@ def get_inventory_levels_of_group_warehouse(warehouse: str, integration: str):
 				ON ei.erpnext_item_code = bin.item_code
 			WHERE bin.warehouse in ({', '.join(['%s'] * len(all_warehouses))})
 				AND integration = %s 
-				AND ei.erpnext_item_code = 'TD-941NOVA'
 			GROUP BY
 				ei.erpnext_item_code
-			ORDER BY last_synced DESC
+			ORDER BY last_synced ASC
 			""",
 		values=all_warehouses + (integration,),
 		as_dict=1,
